a4ad03c20c77ef4e7094d9fb8475e9dd0443614a
HaoEarm <124886637+HaoEarm@users.noreply.github.com>
2024-10-10 15:58:41 +0800

    Added random forest model scripts
    RF_train.py trains a random forest model and saves it in /model as a joblib dump, RF_test.py loads a pre-trained model and runs prediction on the initial test data set, RF_predict.py loads a pre-trained model and  runs prediction based on a given data set.

---
+++ scripts/RF_predict.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+
+import joblib
+import sys
+import os
+
+# Loading the pretrained Random Forest Model
+dirname = os.path.dirname(__file__) # Path to this predict.py
+filename = os.path.join(dirname, '../model/random_forest.joblib') # Relative path to model folder
+rf = joblib.load(rf, filename)
+
+# Loading scaler used from training
+filename2 = os.path.join(dirname, '../model/scaler.joblib') # Relative path to model folder
+scaler = joblib.load(filename2)
+
+# Reading in the parsed data
+df = pd.read_csv('../data/test_data.csv') # Edit File name to relevant csv file
+
+# Drop features that are not needed / redundant
+X = df.drop(columns=['label','gene_id','transcript_id','Key'])
+
+# Scale new input data
+X_scaled = scaler.transform(X)
+
+# Run model predictions
+test_probabilities = rf.predict_proba(X_scaled)[:, 1]
+
+# Create output submission file in required format
+# transcript_id, transcript_position, score
+output = df[['transcript_id', 'transcript_position']]
+
+# Add in prediction scores as 'score' column
+output['score'] = pd.Series(test_probabilities)
+
+# Saving Final output file for submission
+output.to_csv('../output/output.csv', index=False)
+
+print("output scores have been generated, predict script has finished running.")
---
+++ scripts/RF_train.py
@@ -0,0 +1,74 @@
+# Importing Libraries
+import pandas as pd
+import numpy as np
+import ast
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, roc_auc_score, accuracy_score
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from imblearn.over_sampling import SMOTE
+from sklearn.preprocessing import StandardScaler
+
+import os
+import joblib
+
+# Reading in the parsed data
+df = pd.read_csv('../Data/processed_data.csv')
+train_data  = pd.read_csv('../Data/train_data.csv')
+test_data  = pd.read_csv('../Data/test_data.csv')
+val_data  = pd.read_csv('../Data/val_data.csv')
+
+# Drop features that are not needed / redundant
+# Split into X and y (Features and Label)
+X_train = train_data.drop(columns=['label','gene_id','transcript_id','Key'])
+y_train = train_data['label']
+
+X_val = val_data.drop(columns=['label','gene_id','transcript_id','Key'])
+y_val = val_data['label']
+
+X_test = test_data.drop(columns=['label','gene_id','transcript_id','Key'])
+y_test = test_data['label']
+
+# Addressing class imbalance in the Dataset using SMOTE + class weight + scale
+# More Positive cases than Negative Cases
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_val_scaled = scaler.transform(X_val)
+X_test_scaled = scaler.transform(X_test)
+smote = SMOTE(random_state=42)
+X_train_resampled, y_train_resampled = smote.fit_resample(X_train_scaled, y_train)
+
+# Begin Training the Model
+rf = RandomForestClassifier(class_weight='balanced')
+rf.fit(X_train_resampled, y_train_resampled)
+print("Random Forest Model has finished training")
+
+# Running on validation data
+val_predictions = rf.predict(X_val_scaled)
+val_probabilities = rf.predict_proba(X_val_scaled)[:, 1]
+val_accuracy = accuracy_score(y_val, val_predictions)
+val_roc_auc = roc_auc_score(y_val, val_probabilities)
+
+print(f'Validation ROC AUC Score: {val_roc_auc:.2f}')
+print(f'Validation Accuracy Score: {val_accuracy:.2f}')
+
+# Running on test data
+test_predictions = rf.predict(X_test_scaled)
+test_probabilities = rf.predict_proba(X_test_scaled)[:, 1]
+test_accuracy = accuracy_score(y_test, test_predictions)
+test_roc_auc = roc_auc_score(y_test, test_probabilities)
+
+print(f"Test ROC AUC: {test_roc_auc:.2f}")
+print(f"Test Accuracy: {test_accuracy:.2f}")
+
+# Saving the trained Model
+dirname = os.path.dirname(__file__) # Path to this train.py
+filename = os.path.join(dirname, '../model/random_forest.joblib') # Relative path to model folder
+joblib.dump(rf, filename, compress=9)
+
+# Saving Standard Scaler for future Predictions
+filename2 = os.path.join(dirname, '../model/scaler.joblib') # Relative path to model folder
+joblib.dump(scaler, filename2)
+
+print("Random Forest Model has been saved, training script has finished running.")
